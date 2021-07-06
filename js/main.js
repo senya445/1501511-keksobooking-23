f952a5205437c1c126c139ac7e0168a4619edf42
Сеня <mr.komarovars@gmail.com>
2021-07-06 11:49:57 +0300

    все поменял

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-<script>
-  document.addEventListener('DOMContentLoaded', () => alert("Дерево DOM готово после скрипта с 'defer'!")); // (2)
-</script>
-
-<script defer src="https://javascript.info/article/script-async-defer/long.js?speed=1"></script>
+<P>что то не нужне </P>
+  <script defer src="https://javascript.info/article/script-async-defer/long.js?speed=1"></script>
+<P>что то не нужне </P>
