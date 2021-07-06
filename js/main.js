db8eac4619d88a7e30f2b6556a97b425d0b9d8f4
Сеня <mr.komarovars@gmail.com>
2021-07-06 11:42:32 +0300

    добавил функцию

---
+++ js/main.js
@@ -0,0 +1,5 @@
+script>
+  document.addEventListener('DOMContentLoaded', () => alert("Дерево DOM готово после скрипта с 'defer'!")); // (2)
+</script>
+
+<script defer src="https://javascript.info/article/script-async-defer/long.js?speed=1"></script>
